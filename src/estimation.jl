f013b9d286c000e70196635e902c05f72ec4bd05
Gilles Peiffer <gilles.peiffer@outlook.com>
2019-04-17 02:33:07 +0200

    start working on better plots, fix #2

--- src/estimation.jl
+++ src/estimation.jl
@@ -1,7 +1,9 @@
 using Compat
 using Random, Distributions, StatsBase
 using JuMP, Ipopt, SpecialFunctions, LinearAlgebra
-using Gadfly, Cairo, Fontconfig
+# using Plots; pgfplots()
+using PGFPlotsX
+using LaTeXStrings
 
 """
 	mom(V::Vector)
@@ -94,6 +96,7 @@ function d()
 		s_ml_std[i] = std(s_ml[i, :])
 	end
 
+	"""
 	p_k = plot(
 	layer(x=x, y=k_mom_mean, ymin=k_mom_mean - k_mom_std, ymax=k_mom_mean + k_mom_std, Geom.line, Geom.errorbar, Theme(major_label_font="CMU Serif",minor_label_font="CMU Serif",major_label_font_size=16pt,minor_label_font_size=14pt, default_color=colorant"blue")),
 	layer(x=x, y=k_ml_mean, ymin=k_ml_mean - k_ml_std, ymax=k_ml_mean + k_ml_std, Geom.line, Geom.errorbar, Theme(major_label_font="CMU Serif",minor_label_font="CMU Serif",major_label_font_size=16pt,minor_label_font_size=14pt, default_color=colorant"orange")), Guide.xlabel("Size of sample vector"), Guide.ylabel("Mean and standard deviation"), Guide.title("Estimation of the first parameter (k)"), Guide.manual_color_key("Estimators", ["MOM", "ML"], ["blue", "orange"])
@@ -106,6 +109,7 @@ function d()
 
 	p_k |> PNG("k.png")
 	p_s |> PNG("s.png")
+	"""
 end
 
 """
@@ -116,7 +120,7 @@ end
 function f()
 	Γ =  Gamma(1, 2)
 	Mf = 10000
-	N = [10 50 150 3000]
+	N = [10; 50; 150; 3000]
 
 	k_ml_f = zeros(length(N), Mf)
 	s_ml_f = zeros(length(N), Mf)
@@ -155,13 +159,20 @@ function f()
 		centered_ratio_matrix = (cov_matrix ./ fisher_inverse) .- 1.0
 		ratio[index] = opnorm(centered_ratio_matrix, 2) # Spectral norm of the matrix.
 
-		println(n)
 		index += 1
 	end
 
-	p_ratio = plot(x=N, y=ratio, Geom.line, Theme(major_label_font="CMU Serif",minor_label_font="CMU Serif",major_label_font_size=24pt,minor_label_font_size=21pt), Guide.xlabel("Size of sample vector"), Guide.ylabel("Spectral norm of the ratio matrix - [1 1; 1 1]"), Guide.title("Numerical indication of convergence towards the Cramér--Rao bound"));
-
-	p_ratio |> PNG("ratio.png")
+	p_ratio = @pgf Axis(
+	{
+		grid="major",
+		title="\\Large Convergence towards the Cramér--Rao bound",
+		xlabel="\\footnotesize Size of the sample vector",
+		ylabel=L"\footnotesize Spectral norm of $\cov \hTheta \oslash \mathcal{I}^{-1}(\vartheta) - \left(\begin{smallmatrix}1&1\\1&1\end{smallmatrix}\right)$",
+		xmode="log"
+	},
+	Plot(Table([:x => N, :y => ratio]))
+	)
+	pgfsave("ratio.tex", p_ratio, include_preamble = false)
 end
 
 """
@@ -170,9 +181,8 @@ end
 	Main function to group execution.
 """
 function main()
-	set_default_plot_size(32cm, 18cm)
-	d()
-	# f()
+	# d()
+	f()
 end
 
 main()
